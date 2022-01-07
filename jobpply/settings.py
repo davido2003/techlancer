1de1ddc3142d898e50edf730ae4b78898e00e0f0
davido2003 <davidoiwajomo@gmail.com>
2022-01-07 14:01:47 -0800

    .

--- jobpply/settings.py
+++ jobpply/settings.py
@@ -78,25 +78,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'jobpply.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+Database
+https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'HOST': 'localhost',
-#         'NAME': 'portal_db',
-#         'USER': 'portal_user',
-#         'PASSWORD': 'portal_pass',
-#         'PORT': 5432,
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'portal_db',
+        'USER': 'portal_user',
+        'PASSWORD': 'portal_pass',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
